2f3f8d6a520e17314c9f71de025d9f9937a7b1a8
noffie <noffie.github@icloud.com>
2024-05-15 22:54:50 +0200

    added cini submodule

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
-#define CINI_NO_CPP
+#define CINI_NO_CLASS_SYSTEM
 #define CINI_IMPLEMENTATION
-#include <cini.h>
+#include <cini/cini.h>
+
+int libexample(int argc, char** argv) {
+
+}
+
+char* getifarg(int argn, int argc, char** argv) {
+	
+}
 
 int main(int argc, char** argv) {
 	for(int i = 0; i < argc; i++) {
 		printf("%s\n", argv[i]);
 	}
-
+	
 	
 
 	return 0;
